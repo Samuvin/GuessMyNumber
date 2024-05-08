ca1a3ca9b0206d23c2d89c9ef0495deca53e309e
samuvin <samuvin.j@gmail.com>
2024-05-08 12:32:26 +0530

    Game Logic

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const number = Math.trunc(Math.random()*20)+1;
-document.querySelector('.number').textContent=number;
+const secret_number = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent=secret_number;
 
 document.querySelector('.check').addEventListener('click', ()=>
 {
     const guess=Number(document.querySelector('.guess').value);
     if(!guess)
     {
-        document.querySelector('.message').textContent='🚫No Number!🚫'; 
+        document.querySelector('.message').textContent='🚫No Secret_number!🚫'; 
+    }
+    else if(guess===secret_number)
+    {
+        document.querySelector('.message').textContent='🎉🎊Correct Number🎊🎉'
     }
 })
